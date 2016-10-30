c64c3cf64caf5729bf5d2c0ecde082c251e4a94b
Turan Sadri <turan@motleyagency.com>
2016-10-30 21:51:14 +0200

    Date list format updated, dates added, styles

--- src/components/DateSelector.js
+++ src/components/DateSelector.js
@@ -1,36 +1,53 @@
 import React from "react";
 import Link from "react-router/Link";
 
+const moment = require("moment");
+
 class DateSelector extends React.Component {
   render() {
     const dates = [
-      "03-10-2016",
-      "10-10-2016",
-      "17-10-2016",
-      "24-10-2016",
-      "31-10-2016",
-      "07-11-2016",
-      "14-11-2016",
-      "21-11-2016",
-      "28-11-2016",
-      "05-12-2016",
-      "12-12-2016",
-      "19-12-2016",
-      "26-12-2016",
-      "02-01-2016",
-      "09-01-2016",
-      "16-01-2016",
-      "23-01-2016",
-      "30-01-2016",
-      "06-02-2016",
-      "13-02-2016",
-      "20-02-2016",
-      "27-02-2016",
+      "2016-10-03",
+      "2016-10-10",
+      "2016-10-17",
+      "2016-10-24",
+      "2016-10-31",
+      "2016-11-07",
+      "2016-11-14",
+      "2016-11-21",
+      "2016-11-28",
+      "2016-12-05",
+      "2016-12-12",
+      "2016-12-19",
+      "2016-12-26",
+      "2017-01-02",
+      "2017-01-09",
+      "2017-01-16",
+      "2017-01-23",
+      "2017-01-30",
+      "2017-02-06",
+      "2017-02-13",
+      "2017-02-20",
+      "2017-02-27",
+      "2017-03-06",
+      "2017-03-13",
+      "2017-03-20",
+      "2017-03-27",
+      "2017-04-03",
+      "2017-04-10",
+      "2017-04-17",
+      "2017-04-24",
     ];
     return (
-      <ul>
+      <ul className="date-selector list">
         {
-          dates.map(key => <li key={key}><Link to={`/dates/${key}`}>{key}</Link></li>)
+          dates.map(date =>
+            <li className="fl w-third pa2" key={date}>
+              <Link className="date-styled" to={`/dates/${date}`}>
+                <span className="day">{moment(date).format("D")}</span>
+                <span className="month">{moment(date).format("MMMM")}</span>
+                <span className="year">{moment(date).format("YYYY")}</span>
+              </Link>
+            </li>)
         }
       </ul>
     );
